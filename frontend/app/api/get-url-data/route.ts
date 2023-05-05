92f8ffc0ebfca5de920b99d5449a2de00713552e
Javaughn Pryce <javanpryce1@gmail.com>
2023-05-05 01:28:23 -0400

    api routes to fetch data from github api

---
+++ frontend/app/api/get-url-data/route.ts
@@ -0,0 +1,25 @@
+import { ProfileTypes } from "@/utils/projectTypes";
+import { NextResponse } from "next/server";
+import { Octokit } from "octokit";
+
+export async function GET(request: Request) {
+  const octokit = new Octokit({
+    auth: process.env.NEXT_PUBLIC_GITHUB_API_KEY,
+  });
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get("url");
+  try {
+    const res = await octokit.request(`GET ${url?.split("{")[0]}`);
+    const data = res.data;
+    return NextResponse.json(data as ProfileTypes, {
+      status: 200,
+    });
+  } catch {
+    return NextResponse.json(
+      { message: "Requested data not found!" },
+      {
+        status: 404,
+      }
+    );
+  }
+}
